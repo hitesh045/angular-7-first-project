d6c14d5ca75391bd7b80d0e88e04d7444c96e9d2
Hitesh <hitesh.chaudhari@wohlig.com>
2019-12-05 11:53:39 +0530

    second git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { XyzComponent } from './xyz/xyz.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'xyz', component: XyzComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'service', component: ServiceComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
